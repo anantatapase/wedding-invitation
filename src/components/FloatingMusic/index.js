0fc65c40e1c38387bc8acea645f82e18f59ae034
Ananta Tapase <ananta.tapase@gojek.com>
2023-03-21 02:37:31 +0530

    Add thankyou section and reference

--- src/components/FloatingMusic/index.js
+++ src/components/FloatingMusic/index.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import IconMusic from '@assets/images/music-icon.png';
 import IconMusicStop from '@assets/images/music-stop-icon.png';
-import MusicBacksound from '@assets/music/jashn-e-bahaaraa.mp3';
+import MusicBacksound from '@assets/music/jashn-e-baharaa.mp3';
 
 import { styMusicFloating } from './styles';
 
